d019263df8af5595dcc7d887ad6bc898d776a7b4
wwiec <wade.wiechenthal@outlook.com>
2022-02-21 17:03:56 -0500

    Updates version codes

--- app/src/main/java/com/example/wordsapp/MainActivity.kt
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -17,6 +17,7 @@ package com.example.wordsapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wordsapp.databinding.ActivityMainBinding
